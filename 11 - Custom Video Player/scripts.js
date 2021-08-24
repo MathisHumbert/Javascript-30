edb1bdb1b07b942c6cea892c1875c152988c1777
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-08-24 17:46:46 +0200

    redoing project

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -7,48 +7,59 @@ const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
+let flag = false;
 // build our functions
 function togglePlay() {
-  const method = video.paused ? 'play' : 'pause';
-  video[method]();
-}
-function updateButton() {
-  const icon = this.paused ? '►' : '❚ ❚';
-  toggle.textContent = icon;
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
 }
 
-function skip() {
+function toggleLogo() {
+  video.paused ? (toggle.textContent = '►') : (toggle.textContent = '❚ ❚');
+}
+
+function inputChange() {
+  let value = this.value;
+  let name = this.name;
+  console.log(name, value);
+  video[name] = value;
+}
+
+function skipTime() {
   video.currentTime += parseFloat(this.dataset.skip);
 }
 
-function handleRangeUpdate() {
-  video[this.name] = this.value;
-}
-
 function handleProgress() {
-  const percent = (video.currentTime / video.duration) * 100;
-  progressBar.style.flexBasis = `${percent}%`;
+  let time = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${time}%`;
 }
 
-function scrub(e) {
-  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
-  video.currentTime = scrubTime;
-  console.log(scrubTime);
+function mouseProgress(e) {
+  if (flag) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+  }
 }
-
 // hook up the event listeners
 video.addEventListener('click', togglePlay);
-video.addEventListener('play', updateButton);
-video.addEventListener('pause', updateButton);
+video.addEventListener('pause', toggleLogo);
+video.addEventListener('play', toggleLogo);
 video.addEventListener('timeupdate', handleProgress);
 toggle.addEventListener('click', togglePlay);
-skipButtons.forEach((skipButton) => skipButton.addEventListener('click', skip));
-ranges.forEach((range) => {
-  range.addEventListener('input', handleRangeUpdate);
+toggle.addEventListener('click', toggleLogo);
+
+ranges.forEach((btn) => {
+  btn.addEventListener('input', inputChange);
+});
+skipButtons.forEach((btn) => {
+  btn.addEventListener('click', skipTime);
 });
 
-let mousedown = false;
-progress.addEventListener('mousedown', () => (mousedown = true));
-progress.addEventListener('mouseup', () => (mousedown = false));
-progress.addEventListener('click', scrub);
-progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousemove', mouseProgress);
+progress.addEventListener('mousedown', () => (flag = true));
+progress.addEventListener('mouseup', () => (flag = false));
+
+// progressBar.style.flexBasis =
