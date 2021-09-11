e0c7437a8449950cd845126cfe4194a79546fd0a
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-09-11 16:40:01 +0200

    redoing project

--- 29 - Countdown Timer/scripts.js
+++ 29 - Countdown Timer/scripts.js
@@ -6,10 +6,10 @@
 // }
 
 let countdown;
+
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
 const buttons = document.querySelectorAll('[data-time]');
-
 function timer(seconds) {
   // clear any existing timer
   clearInterval(countdown);
